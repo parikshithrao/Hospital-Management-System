1c1bb9cd1cadb6fb7a6e0a213c6bcc57010f2a42
Parikshith H <parikshithrao15@gmail.com>
2020-10-31 21:44:40 +0530

    modified

--- my_app/models.py
+++ my_app/models.py
@@ -43,5 +43,15 @@ class Doctor(AbstractBaseUser):
     REQUIRED_FIELDS = []
     objects = UserManager()
 
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
 
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
 
+    def __str__(self):
+        return self.fname
