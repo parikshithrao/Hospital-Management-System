5a2d9101766cdaadcae30fcf0a6a68a327fdf1a1
Parikshith H <parikshithrao15@gmail.com>
2021-01-03 09:39:33 +0530

    superuser created

---
+++ my_app/management/commands/create_superuser.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+
+from my_app.models import DoctorRegister
+
+
+class Command(BaseCommand):
+    help = 'Create Customer users'
+
+    def add_arguments(self, parser):
+        parser.add_argument('phone', type=str, help='PHONE')
+        parser.add_argument('email', type=str, help='EMAIL')
+        parser.add_argument('password', type=str, help='PASSWORD')
+        parser.add_argument('first_name', type=str, help='FIRST_NAME')
+
+
+
+
+    def handle(self, *args, **kwargs):
+        phone = kwargs['phone']
+        password = kwargs['password']
+        email = kwargs['email']
+        first_name = kwargs['first_name']
+        user = DoctorRegister()
+        user.phone = phone
+        user.is_staff = True
+        user.is_active = True
+        user.set_password(password)
+        user.email = email
+        user.first_name = first_name
+        user.save()
+
+        self.stdout.write("SuperUser added successfully.")
